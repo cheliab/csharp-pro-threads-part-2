9666c8b6e38957bc5dd00ebf5b9e359118e7ce87
Berezkin Pavel <cheliabkns@gmail.com>
2021-12-03 23:01:49 +1000

    Добавил код 2 примера - Статические методы VolatileWrite и VolatileRead

--- N_002_Volatile_StaticMethods/Program.cs
+++ N_002_Volatile_StaticMethods/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Альтернативный способ работы с volatile
+// Статические методы VolatileWrite() и VolatileRead()
+public class Program 
+{
+    // теперь тут можно не использовать volatile
+    static int stop;
+
+    private static void Function()
+    {
+        int x = 0;
+
+        while (Thread.VolatileRead(ref stop) != 1)
+        {
+            x++;
+        }
+
+        Console.WriteLine($"Function: поток остановлен при x = {x}");
+    }
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("Main: Start");
+        Thread thread = new Thread(Function);
+        thread.Start();
+
+        Thread.Sleep(2000);
+
+        Thread.VolatileWrite(ref stop, 1); // stop = 1;
+        Console.WriteLine("Main: ожидание завершения вторичного потока.");
+
+        thread.Join();
+
+        Console.ReadKey();
+    }
+}
+
+// Результат:
+
+//Main: Start
+//Main: ожидание завершения вторичного потока.
+//Function: поток остановлен при x = 2126732830
