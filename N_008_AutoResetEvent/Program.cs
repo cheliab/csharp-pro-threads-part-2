576f6d35ebd82c73b587f470385bff9c039e414f
Berezkin Pavel <cheliabkns@gmail.com>
2021-12-11 21:37:50 +1000

    Добавил пример 8 - AutoResetEvent

---
+++ N_008_AutoResetEvent/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// AutoResetEvent - Уведомляет ожидающий поток о том, что произошло событие.
+
+class Program
+{
+    /// <summary>
+    /// Аргумент:
+    /// initialState = false - установка в несигнальное состояние.
+    /// initialState = true - эквивалентно вызову _auto.Set();
+    /// </summary>
+    private static AutoResetEvent _auto = new AutoResetEvent(false);
+    
+    public static void Main()
+    {
+        Thread thread = new Thread(Function);
+        thread.Start();
+        Thread.Sleep(500); // Время для запуска вторичного потока
+
+        Console.WriteLine("Нажмите на любую клавишу - AutoResetEvent в сигнальное состояние");
+        Console.ReadKey();
+
+        _auto.Set(); // Продолжить выполнять вторичный поток
+        
+        Console.WriteLine("Нажмите на любую клавишу - AutoResetEvent в сигнальное состояние");
+        Console.ReadKey();
+
+        _auto.Set(); // Продолжить выполнять вторичный поток
+        
+        Console.ReadKey();
+    }
+
+    private static void Function()
+    {
+        Console.WriteLine("Красный свет");
+        _auto.WaitOne(); // Остановка выполнения вторичного потока.
+        
+        Console.WriteLine("Желтый");
+        _auto.WaitOne(); // Остановка выполнения вторичного потока.
+        
+        Console.WriteLine("Зеленый");
+    }
+}
+
