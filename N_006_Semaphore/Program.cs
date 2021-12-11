7866777bf729f9bfc2fc5cb785efd5c245fec374
Berezkin Pavel <cheliabkns@gmail.com>
2021-12-11 15:07:24 +1000

    6 проект - Добавил пример в README с запуском 3 приложений одновременно

--- N_006_Semaphore/Program.cs
+++ N_006_Semaphore/Program.cs
@@ -27,6 +27,8 @@ public class Program
     
     public static void Main()
     {
+        Console.WindowWidth = 40;
+        
         /*
          * Аргументы конструктора:
          * initialCount - задает начальное количество потоков которое может входить в семафор
@@ -34,15 +36,15 @@ public class Program
          * name - название семафора в операционной системе (межпроцессное взаимодействие)
          */
         
-        _pool = new Semaphore(2, 4, "MySemaphore");
+        _pool = new Semaphore(5, 7, "MySemaphore");
 
-        _pool.Release(2); // Сбросить занятые слоты (2), всего свободно 4
+        //_pool.Release(2); // Сбросить занятые слоты (2), всего свободно 4
 
-        for (int i = 1; i <= 8; i++)
+        for (int i = 1; i <= 40; i++)
         {
             new Thread(Function).Start(i);
             
-            // Thread.Sleep(500); // Чтобо потоки разных процессов могли перемешаться
+            Thread.Sleep(500); // Чтобо потоки разных процессов могли перемешаться
         }
         
         Console.ReadKey();
