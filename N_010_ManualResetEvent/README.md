c20f1381a94661a7328241d4c9088317941c55ff
Berezkin Pavel <cheliabkns@gmail.com>
2021-12-18 18:49:59 +1000

    Добавил 11 пример - ManualResetEventSlim

---
+++ N_010_ManualResetEvent/README.md
@@ -0,0 +1,7 @@
+﻿Отличие работы между AutoResetEvent и ManualResetEvent
+
+![](img/1.png)
+
+Результат работы
+
+![](img/1.gif)
---
+++ N_011_ManualResetSlim/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace N_011_ManualResetSlim;
+
+public static class Program
+{
+    /// <summary>
+    /// initialState = false - не сигнальное состояние
+    /// </summary>
+    private static ManualResetEventSlim _eventSlim = new ManualResetEventSlim(false);
+    
+    public static void Main()
+    {
+        new Thread(Function1).Start();
+        new Thread(Function2).Start();
+        
+        Thread.Sleep(500);
+
+        Console.WriteLine("Нажмите клавишу, чтобы разблокировали потоки");
+        Console.ReadKey();
+        _eventSlim.Set();
+        
+        Console.ReadKey();
+    }
+
+    private static void Function1()
+    {
+        Console.WriteLine("Поток 1 запущен и ожидает сигнала.");
+        _eventSlim.Wait();
+        Console.WriteLine("Поток 1 завершается.");
+    }
+
+    private static void Function2()
+    {
+        Console.WriteLine("Поток 2 запущен и ожидает сигнала.");
+        _eventSlim.Wait();
+        Console.WriteLine("Поток 2 завершается.");
+    }
+}
