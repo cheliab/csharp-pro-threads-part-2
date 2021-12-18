bf30a6553481907e5b921af0be44c939649c46ff
Berezkin Pavel <cheliabkns@gmail.com>
2021-12-18 19:06:09 +1000

    Добавил набросок 12 проекта - ManualResetEvent с использованием метода Reset

---
+++ N_012_ManualResetEvent_ResetMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace N_012_ManualResetEvent_ResetMethod;
+
+class Program
+{
+    private static ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+
+    private static void Function()
+    {
+        
+    }
+    
+    public static void Main()
+    {
+        
+    }
+}
