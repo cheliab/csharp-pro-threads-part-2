519c6701157b182021580e9160c7298747429913
Berezkin Pavel <cheliabkns@gmail.com>
2021-12-19 15:42:48 +1000

    Доделал 12 проект

--- N_012_ManualResetEvent_ResetMethod/Program.cs
+++ N_012_ManualResetEvent_ResetMethod/Program.cs
@@ -2,15 +2,52 @@
 
 class Program
 {
+    /// <summary>
+    /// initialState = false - установка в несигнальное состояние
+    /// </summary>
     private static ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
 
     private static void Function()
     {
+        Console.WriteLine("Запущен поток {0}", Thread.CurrentThread.Name);
+
+        for (int i = 0; i < 80; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(20);
+        }
         
+        Console.WriteLine("Завершен поток {0}", Thread.CurrentThread.Name);
+
+        _manualResetEvent.Set(); // Сигнал для разблокировки первичного потока
     }
     
     public static void Main()
     {
+        Thread thread = new Thread(Function) // 1-й поток
+        {
+            Name = "1"    
+        };
+        thread.Start();
+
+        Console.WriteLine("Приостановка выполнения первичного потока.");
+        _manualResetEvent.WaitOne();
         
+        Console.WriteLine("Первичный поток возобновил работу.");
+
+        // _manualResetEvent.Reset(); // ручная установка в несигнальное состояние (AutoResetEvent - делает это автоматически)
+
+        thread = new Thread(Function) // 2-й поток
+        {
+            Name = "2"
+        };
+        thread.Start();
+        
+        Console.WriteLine("Приостановка выполнения первичного потока.");
+        _manualResetEvent.WaitOne();
+        
+        Console.WriteLine("Первичный поток возобновил и завершил работу.");
+
+        Console.ReadKey();
     }
 }
---
+++ N_012_ManualResetEvent_ResetMethod/README.md
@@ -0,0 +1,18 @@
+﻿![](img/1.gif)
+
+Пример отключения <b>manualResetEvent.Set()</b>
+
+![](img/Screenshot_1.png)
+
+Результат работы.
+Первичный поток не возобновляет работу.
+
+![](img/2.gif)
+
+Пример отключения <b>manualResetEvent.Reset()</b>
+
+![](img/Screenshot_2.png)
+
+Первичный поток быстрее завершает свою работу.
+
+![](img/3.gif)
